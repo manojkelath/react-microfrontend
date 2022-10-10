a568edd346059577eed45e216d0484311b7445fb
Manoj Kelath <manojkelath1987@gmail.com>
2022-10-09 22:03:08 -0400

    add dynamic prod vs dev for host

--- host/src/index.js
+++ host/src/index.js
@@ -1,5 +1,8 @@
 // Use dynamic import here to allow webpack to interface with module federation code
-window.remote1Url = 'http://localhost:3001';
-window.libsUrl = 'http://localhost:3002';
+window.remote1UrlProd = 'http://localhost:3001';
+window.libsUrlProd = 'http://localhost:3002';
+
+window.remote1UrlDev = 'http://localhost:3001';
+window.libsUrlDev = 'http://localhost:3002';
 
 import('./bootstrap');
--- host/webpack.config.js
+++ host/webpack.config.js
@@ -4,51 +4,55 @@ const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
 const path = require('path');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
-module.exports = {
-  entry: './src/index',
-  mode: 'development',
-  devtool: 'source-map',
-  optimization: {
-    minimize: false,
-  },
-  devServer: {
-    hot: false,
-    static: path.join(__dirname, 'dist'),
-    port: 3000,
-    historyApiFallback: {
-      index: 'index.html',
+module.exports = env => {
+
+  return {
+    entry: './src/index',
+    mode: 'development',
+    devtool: 'source-map',
+    optimization: {
+      minimize: false,
     },
-  },
-  output: {
-    publicPath: 'auto',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-react'],
+    devServer: {
+      hot: false,
+      static: path.join(__dirname, 'dist'),
+      port: 3000,
+      historyApiFallback: {
+        index: 'index.html',
+      },
+    },
+    output: {
+      publicPath: 'auto',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: {
+            presets: ['@babel/preset-react'],
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'host',
+        remotes: {
+          remote1: env.prod ? 'remote1@[remote1UrlProd]/remoteEntry.js' : 'remote1@[remote1UrlDev]/remoteEntry.js',
+          libs: 'libs@[libsUrl]/remoteEntry.js',
+        },
+      }),
+      new ExternalTemplateRemotesPlugin(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new LiveReloadPlugin({
+        port: 35729,
+      }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'host',
-      remotes: {
-        remote1: 'remote1@[remote1Url]/remoteEntry.js',
-        libs: 'libs@[libsUrl]/remoteEntry.js',
-      },
-    }),
-    new ExternalTemplateRemotesPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new LiveReloadPlugin({
-      port: 35729,
-    }),
-  ],
+  }
+
 };
