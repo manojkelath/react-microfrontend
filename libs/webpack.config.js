339136063fd9c605ab3a66b7c151b128f1f7b6c3
Manoj Kelath <manojkelath1987@gmail.com>
2022-10-16 00:27:44 -0400

    added chkra ui

--- libs/webpack.config.js
+++ libs/webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
         './react': 'react',
         './react-dom/client': 'react-dom/client',
         './react-dom': 'react-dom',
+        './@chakra-ui/react': '@chakra-ui/react',
+        './@chakra-ui/icons': '@chakra-ui/icons',
         './react-router-dom': 'react-router-dom',
       },
     }),
